060fa00d03d30215613d4fdea96e07eacf26751f
Joe Farrington <farrington.joe@gmail.com>
2023-11-13 15:38:30 +0000

    Move logenvwrapper functionality to utils

--- bloodbank_marl/ppo_multi_agent/run_ppo_demoor.py
+++ bloodbank_marl/ppo_multi_agent/run_ppo_demoor.py
@@ -12,6 +12,7 @@ from bloodbank_marl.scenarios.de_moor_perishable.jax_env import DeMoorPerishable
 from bloodbank_marl.policies import replenishment, issuing, policy_manager, common
 from bloodbank_marl.utils.gymnax_fitness import GymnaxFitness
 from bloodbank_marl.utils.rollout_manager import RolloutManager
+from bloodbank_marl.utils.gymnax_wrappers import LogEnvState, LogWrapper, LogInfo
 import flax.linen as nn
 import numpy as np
 import optax
@@ -33,88 +34,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly
-
-
-class GymnaxWrapper(object):
-    """Base class for Gymnax wrappers."""
-
-    def __init__(self, env):
-        self._env = env
-
-    # provide proxy access to regular attributes of wrapped object
-    def __getattr__(self, name):
-        return getattr(self._env, name)
-
-
-@struct.dataclass
-class LogEnvState:
-    env_state: environment.EnvState
-    episode_returns: float
-    episode_lengths: int
-    returned_episode_returns: float
-    returned_episode_lengths: int
-    timestep: int
-
-
-class LogWrapper(GymnaxWrapper):
-    """Log the episode returns and lengths."""
-
-    # TODO This is customised for our setup, so we should probably give it a more specific name
-    # TODO: Check discounting of new_ep_return by using known policies.
-
-    def __init__(self, env: environment.Environment):
-        super().__init__(env)
-
-    @partial(jax.jit, static_argnums=(0,))
-    def reset(
-        self, key: chex.PRNGKey, params: Optional[environment.EnvParams] = None
-    ) -> Tuple[chex.Array, environment.EnvState]:
-        obs, env_state = self._env.reset(key, params)
-        state = LogEnvState(env_state, 0, 0, 0, 0, 0)
-        return obs, state
-
-    @partial(jax.jit, static_argnums=(0,))
-    def step(
-        self,
-        key: chex.PRNGKey,
-        state: environment.EnvState,
-        action: Union[int, float],
-        params: Optional[environment.EnvParams] = None,
-    ) -> Tuple[chex.Array, environment.EnvState, float, bool, dict]:
-        obs, env_state, reward, truncation, termination, _ = self._env.step(
-            key, state.env_state, action, params
-        )
-        done = jax.lax.eq(env_state.live_agents.sum(axis=-1), 0)
-        new_episode_return = state.episode_returns + (
-            reward
-            * (1 - env_state.agent_id)
-            * (params.gamma ** jnp.clip(env_state.day - 1, 0, None))
-        )  # So we only capture the reward of the first agent
-        new_episode_length = state.episode_lengths + 1
-        state = LogEnvState(
-            env_state=env_state,
-            episode_returns=new_episode_return * (1 - done),
-            episode_lengths=new_episode_length * (1 - done),
-            returned_episode_returns=state.returned_episode_returns * (1 - done)
-            + new_episode_return * done,
-            returned_episode_lengths=state.returned_episode_lengths * (1 - done)
-            + new_episode_length * done,
-            timestep=state.timestep + 1,
-        )
-        info = {}
-        info["returned_episode_returns"] = state.returned_episode_returns
-        info["returned_episode_lengths"] = state.returned_episode_lengths
-        info["timestep"] = state.timestep
-        info["returned_episode"] = done
-        return obs, state, reward, truncation, termination, info
-
-
-@struct.dataclass
-class LogInfo:
-    timestep: int
-    returned_episode_returns: float
-    returned_episode_lengths: int
-    returned_episode: bool
 
 
 @struct.dataclass
