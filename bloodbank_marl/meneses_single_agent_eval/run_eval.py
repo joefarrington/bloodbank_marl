c26dc7f2514bef7081f97c849d03f58158e559e9
Joe Farrington <farrington.joe@gmail.com>
2023-11-29 12:58:24 +0000

    Update running evaluation for basic Meneses rep policies

--- bloodbank_marl/meneses_single_agent_eval/run_eval.py
+++ bloodbank_marl/meneses_single_agent_eval/run_eval.py
@@ -1,5 +1,4 @@
 import bloodbank_marl
-from bloodbank_marl.policies.replenishment import SPolicy
 import jax
 import jax.numpy as jnp
 import wandb
@@ -25,62 +24,46 @@ def main(cfg):
 
     run = wandb.init(**wandb_config["wandb"]["init"], config=wandb_config)
     # TODO Avoid having to hardcode the product types here
-    types = ["O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+"]
+
     rep_policy = hydra.utils.instantiate(cfg.policies.replenishment)
     # TODO: Better way of inputting policy parameters
+    types = ["O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+"]
     rpp = hydra.utils.instantiate(cfg.policies.replenishment_policy_params)
     rep_policy_params = jnp.array([rpp[f"S_{t}"] for t in types]).reshape(-1, 1)
-    rw = hydra.utils.instantiate(cfg.rollout_wrapper, model_forward=rep_policy.apply)
 
-    rng = jax.random.PRNGKey(cfg.evaluation.seed)
-    log.info(f"Running {cfg.evaluation.num_rollouts} evaluation rollouts.")
-
-    batch_results = rw.batch_rollout(
-        jax.random.split(rng, cfg.evaluation.num_rollouts), rep_policy_params
+    test_evaluator = hydra.utils.instantiate(cfg.evaluation.test_evaluator)
+    test_evaluator.set_apply_fn(rep_policy.apply)
+    policy_params = jnp.array([rep_policy_params])
+    rng_eval = jax.random.PRNGKey(cfg.evaluation.seed)
+    log.info(
+        f"Running {cfg.evaluation.test_evaluator.num_rollouts} evaluation rollouts."
     )
-    log.info(f"Calcuting metrics.")
-
-    env, env_params = make(cfg.environment.env_name, **cfg.environment.env_params)
-    kpis = jax.vmap(env.calculate_kpis)(batch_results["info"])
+    fitness, cum_infos, kpis = test_evaluator.rollout(rng_eval, policy_params)
 
     # TODO Avoid having to hardcode the different metrics here
-    group_metrics = [
-        "mean_demand_by_pt_blood_group",
-        "mean_order_by_product",
-        "service_level_%_by_pt_blood_group",
-        "expiries_%_by_product",
-        "mean_holding_by_product",
-        "mean_age_at_transfusion_by_pt_blood_group",
-        "exact_match_%_by_pt_blood_group",
-    ]
-
-    overall_metrics = [
-        "mean_total_order",
-        "service_level_%",
-        "expiries_%",
-        "mean_holding",
-        "exact_match_%",
-        "mean_age_at_transfusion",
-        "unmet_demand_units",
-        "expired_units",
-    ]
+    group_metrics = cfg.environment.vector_kpis_to_log
+    overall_metrics = cfg.environment.scalar_kpis_to_log
 
     # Create a dataframe of KPIs by type and log to W&B as a table
     df = pd.DataFrame()
     for m in group_metrics:
-        df = pd.concat([df, pd.DataFrame(kpis[m].mean(axis=0).reshape(1, -1))], axis=0)
-        df = pd.concat([df, pd.DataFrame(kpis[m].std(axis=0).reshape(1, -1))], axis=0)
+        df = pd.concat(
+            [df, pd.DataFrame(kpis[m].mean(axis=(0, 1)).reshape(1, -1))], axis=0
+        )
+        df = pd.concat(
+            [df, pd.DataFrame(kpis[m].std(axis=(0, 1)).reshape(1, -1))], axis=0
+        )
     df.columns = types
     row_labels = [f"{m}_{x}" for m in group_metrics for x in ["mean", "std"]]
     df.insert(loc=0, column="metric", value=row_labels)
-    wandb.log({"group_metrics": wandb.Table(dataframe=df)})
+    wandb.log({"eval/group_metrics": wandb.Table(dataframe=df)})
 
     # Log aggregate metrics to W&B, plus return
     for m in overall_metrics:
         wandb.run.summary[f"{m}_mean"] = kpis[m].mean()
         wandb.run.summary[f"{m}_std"] = kpis[m].std()
-    wandb.run.summary["return_mean"] = batch_results["cum_return"].mean()
-    wandb.run.summary["return_std"] = batch_results["cum_return"].std()
+    wandb.run.summary["eval/return_mean"] = fitness.mean()
+    wandb.run.summary["eval/return_std"] = fitness.std()
     log.info(f"Done.")
 
 
