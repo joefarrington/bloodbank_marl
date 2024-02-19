d335df45411541d02e4a38957b73055571391290
Joe Farrington <farrington.joe@gmail.com>
2024-02-19 14:56:51 +0000

    Utils to load params from heuristic checkpoint

--- bloodbank_marl/utils/load_save_policy_params.py
+++ bloodbank_marl/utils/load_save_policy_params.py
@@ -1,5 +1,6 @@
 from orbax.checkpoint import CheckpointManager
 import jax
+import jax.numpy as jnp
 
 
 def load_flax_policy_params(
@@ -8,4 +9,10 @@ def load_flax_policy_params(
     """Load policy parameters from a checkpoint and reshape for use with our fitness functions"""
     policy_params = checkpoint_manager.restore(checkpoint_id)[param_key]
     policy_params = jax.tree_map(lambda x: x.reshape((1,) + x.shape), policy_params)
+    return policy_params
+
+
+def load_heuristic_policy_params(filepath: str):
+    """Load policy parameters from a file, by default when saved from simopt will already be ready for use in fitness functions"""
+    policy_params = jnp.load(filepath)
     return policy_params
