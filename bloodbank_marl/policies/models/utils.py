2dca6519982abb05e9776877a8456a2c0fe4986d
Joe Farrington <farrington.joe@gmail.com>
2024-02-23 15:44:20 +0000

    Additional preproc helper functions

--- bloodbank_marl/policies/models/utils.py
+++ bloodbank_marl/policies/models/utils.py
@@ -8,3 +8,11 @@ import jax.numpy as jnp
 
 def total_stock_on_hand_and_in_transit_by_product(obs):
     return jnp.sum(obs.stock, axis=-1) + jnp.sum(obs.in_transit, axis=-1)
+
+
+def rep_obs(obs):
+    return obs.rep_obs
+
+
+def one_hot_day_of_week(obs):
+    return obs.obs_with_one_hot_day_of_week
