bc49d4dadc950fa332893b44ecfcfea8ef5825d1
Joe Farrington <farrington.joe@gmail.com>
2024-02-26 14:47:46 +0000

    Update PPO models to take same n_hidden args as simopt NNs, and default activation to relu

--- bloodbank_marl/policies/models/flax_stochastic.py
+++ bloodbank_marl/policies/models/flax_stochastic.py
@@ -11,13 +11,14 @@ import numpy as np
 import chex
 from flax import struct
 from bloodbank_marl.utils.gymnax_fitness import make
+from typing import Union, Dict, Optional, Any, List
 
 
 class DiscreteActorCritic(nn.Module):
+    n_hidden: Union[int, list]
     n_actions: int
-    n_hidden: int = 64
     action_pad: int = 0
-    activation: str = "tanh"
+    activation: str = "relu"
     preprocess_observation: callable = lambda obs: obs.obs
 
     @nn.compact
@@ -27,14 +28,13 @@ class DiscreteActorCritic(nn.Module):
         else:
             activation = nn.tanh
         x = self.preprocess_observation(obs)  # Flat representation
-        actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(actor_mean)
-        actor_mean = activation(actor_mean)
+        n_hidden = [self.n_hidden] if isinstance(self.n_hidden, int) else self.n_hidden
+
+        for h in n_hidden:
+            actor_mean = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            actor_mean = activation(x)
         actor_mean = nn.Dense(
             self.n_actions, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
         )(actor_mean)
@@ -45,14 +45,11 @@ class DiscreteActorCritic(nn.Module):
         actor_mean = actor_mean + jnp.where(obs.action_mask == 0, -1e8, 0.0)
         pi = distrax.Categorical(logits=actor_mean)
 
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        critic = activation(critic)
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(critic)
-        critic = activation(critic)
+        for h in n_hidden:
+            critic = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            critic = activation(x)
         critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
             critic
         )
@@ -61,10 +58,10 @@ class DiscreteActorCritic(nn.Module):
 
 
 class ContinuousActorCritic(nn.Module):
+    n_hidden: Union[int, list]
     n_actions: int
-    n_hidden: int = 64
     action_pad: int = 0  # Not currently used
-    activation: str = "tanh"
+    activation: str = "relu"
     preprocess_observation: callable = lambda obs: obs.obs
 
     @nn.compact
@@ -74,28 +71,24 @@ class ContinuousActorCritic(nn.Module):
         else:
             activation = nn.tanh
         x = self.preprocess_observation(obs)  # Flat representation
-        actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(actor_mean)
-        actor_mean = activation(actor_mean)
+        n_hidden = [self.n_hidden] if isinstance(self.n_hidden, int) else self.n_hidden
+
+        for h in n_hidden:
+            actor_mean = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            actor_mean = activation(x)
         actor_mean = nn.Dense(
             self.n_actions, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
         )(actor_mean)
         actor_logtstd = self.param("log_std", nn.initializers.zeros, (self.n_actions,))
         pi = distrax.MultivariateNormalDiag(actor_mean, jnp.exp(actor_logtstd))
 
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        critic = activation(critic)
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(critic)
-        critic = activation(critic)
+        for h in n_hidden:
+            critic = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            critic = activation(x)
         critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
             critic
         )
@@ -105,10 +98,10 @@ class ContinuousActorCritic(nn.Module):
 
 # # TODO MAybe rename this multiproduct?
 class DiscreteIssuingActorCritic(nn.Module):
+    n_hidden: Union[int, list]
     n_actions: int
-    n_hidden: int = 64
     action_pad: int = 0
-    activation: str = "tanh"
+    activation: str = "relu"
     preprocess_observation: callable = lambda obs: obs.obs
 
     @nn.compact
@@ -118,19 +111,17 @@ class DiscreteIssuingActorCritic(nn.Module):
         else:
             activation = nn.tanh
         x = self.preprocess_observation(obs)  # Flat representation
+        n_hidden = [self.n_hidden] if isinstance(self.n_hidden, int) else self.n_hidden
+
+        for h in n_hidden:
+            actor_mean = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            actor_mean = activation(x)
         actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            self.n_actions, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
         )(actor_mean)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
-            self.n_actions + 1, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
-        )(
-            actor_mean
-        )  # Add 1 to actions, 0 in the categorical dist is issuing nothing
+        # Add 1 to actions, 0 in the categorical dist is issuing nothing
         # Apply action masking to logits
         ones_shape = obs.action_mask.shape[:-1] + (1,)
         ones = jnp.ones(ones_shape)
@@ -139,14 +130,11 @@ class DiscreteIssuingActorCritic(nn.Module):
         )  # Stack the action mask with 1, can always issue nothing
         pi = distrax.Categorical(logits=actor_mean)
 
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(x)
-        critic = activation(critic)
-        critic = nn.Dense(
-            self.n_hidden, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(critic)
-        critic = activation(critic)
+        for h in n_hidden:
+            critic = nn.Dense(
+                h, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            )(x)
+            critic = activation(x)
         critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
             critic
         )
