f608e9239c354a8a50d89ec00a159bddedde41e1
Joe Farrington <farrington.joe@gmail.com>
2024-05-07 19:46:17 +0100

    Correct name of script to run

--- bloodbank_marl/utils/viso_jax/value_iteration/run_all_de_moor_vi.py
+++ bloodbank_marl/utils/viso_jax/value_iteration/run_all_de_moor_vi.py
@@ -5,7 +5,7 @@ experiment_configs = ["m2/exp2", "m2/exp6", "m5/exp2", "m5/exp6"]
 for experiment_config in experiment_configs:
     command = [
         "python",
-        "run_simopt.py",
+        "run_value_iteration.py",
         f"+experiment=de_moor_perishable/{experiment_config}/pareto_vi",
     ]
     subprocess.call(command)
