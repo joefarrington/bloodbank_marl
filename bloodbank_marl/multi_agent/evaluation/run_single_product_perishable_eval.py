f6d49083da30bc3dbb06579a37b465ea5e00fe5a
Joe Farrington <farrington.joe@gmail.com>
2024-06-21 11:42:44 +0100

    Rename single agent benchmark config files and add script for eval

---
+++ bloodbank_marl/multi_agent/evaluation/run_single_product_perishable_eval.py
@@ -0,0 +1,36 @@
+import subprocess
+from typing import List
+
+scenario_settings = ["m2/exp2", "m2/exp6", "m5/exp2", "m5/exp6"]
+
+# Note fit_both_replenishment_direct_action used as comparison in pretraining experiments, rather than main results table
+policies_fit = [
+    "fit_replenishment_order_up_to",
+    "fit_issue",
+    "fit_both_replenishment_order_up_to",
+    "fit_both_replenishment_direct_action",
+]
+fit_method = ["simple_ga", "open_es", "ppo"]
+benchmark = ["value_iteration", "simopt"]
+
+# Benchmarks are replenishment only
+args_per_exp = [
+    [f"+experiment=single_product_perishable/{b}/{s}/rep_policy.yaml"]
+    for s in scenario_settings
+    for b in benchmark
+]
+for args in args_per_exp:
+    command = ["python", "run_eval.py"] + args
+    subprocess.call(command)
+
+# Fitting NN policies
+args_per_exp = [
+    [f"+experiment=single_product_perishable/{p}/{s}/{f}.yaml"]
+    for p in policies_fit
+    for s in scenario_settings
+    for f in fit_method
+]
+
+for args in args_per_exp:
+    command = ["python", "run_eval.py"] + args
+    subprocess.call(command)
